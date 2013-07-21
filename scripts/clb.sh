b221905f662324d31e08c423418087f9f008539d
= <=>
2013-07-21 15:38:24 +0100

    adding clb

---
+++ scripts/clb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# grep build.conf for current live branch of something
+usage="$0 <customer code (lower)> <module>"
+
+if [ "$1" == "" -o "$2" == "" ]
+then
+	echo $usage;
+	exit;
+fi
+
+cust=$1
+module=$2
+
+ssh r "grep $module /morespace/daily_build/support_${cust}*/build.conf | grep pserver"
