3e53e53aca2aef6807ba93e1803fd206370078dc
Stephen Hazleton <shazleton@hotmail.co.uk>
2013-10-16 22:17:30 +0100

    screenshotter and screen bindings (incase we need to swap E and W

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,31 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Actions.PhysicalScreens
+import qualified Data.Map as M
 
-main = do
-  xmonad $ gnomeConfig
+main = xmonad $ gnomeConfig
+	{
+		borderWidth          = 1
+		, terminal           = "urxvt"
+		, normalBorderColor  = "#cccccc"
+		, focusedBorderColor = "#cd8b00"
+        , keys               = screenKeys <+> myKeys <+> keys gnomeConfig
+	}
+
+-- cant figure out the syntax to combine this with anything else
+screenKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+	[
+		--
+		-- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+		-- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+		--
+		((modm .|. mask, key), f sc)
+			| (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+			, (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]
+	]
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+	[
+		-- screenshotter
+		((modm, xK_y), spawn "sleep 0.2; scrot -s '/space/screens/screenshot-%Y%m%d%H%M%S-$wx$h.png' -e 'feh $f'")
+	]
