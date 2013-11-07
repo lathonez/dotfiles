57e12a7cc50e70c43d4c716e5749124ea5be79cd
Stephen Hazleton <stephen.hazleton@openbet.com>
2013-11-07 17:05:24 +0000

    adding oxipub conf here

--- work/docker/rep_client/checkout.sh
+++ work/docker/rep_client/checkout.sh
@@ -23,7 +23,7 @@ cvs co -d conf/OXi/oxipubserver -r B_WillHill_34_0_2 willhill/conf/OXi/oxipubser
 cvs co -d conf/postgres         -r B_WillHill_34_0_2 willhill/conf/postgres
 
 # links
-ln -s $base/conf/OXi/oxipubserver/oxipub.cfg       $base/OXi/services/dbPublish
+ln -s $base/setup/oxipub.cfg                       $base/OXi/services/dbPublish
 ln -s $base/OXi/services/repServer/schema-base.cfg $base/OXi/services/dbPublish
 ln -s $base/OXi/services/repServer/api-base.cfg    $base/OXi/services/dbPublish
 ln -s $base/OXi/shared/tcl                         $base/OXi/services/dbPublish/tcl/shared
