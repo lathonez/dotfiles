8a827e8bfc34d1b53aace8b2e612d1c0b4caf7e1
Stephen Hazleton <stephen.hazleton@openbet.com>
2013-11-14 12:43:02 +0000

    changes moving to release

--- work/docker/rep_client/checkout.sh
+++ work/docker/rep_client/checkout.sh
@@ -1,56 +1,67 @@
 #!/bin/bash
 # mkdirs
-mkdir $OPENBETRELEASE
-mkdir $OPENBETLOGDIR
-mkdir $OPENBETLOGDIR/oxipub
-mkdir $OPENBETLOGDIR/oxirep
-mkdir $OPENBETLOGDIR/rep_client
-mkdir $OPENBETLOGDIR/site_management
 
-cd $OPENBETRELEASE
+checkout() {
+	mkdir $OPENBETRELEASE
+	mkdir $OPENBETLOGDIR
+	mkdir $OPENBETLOGDIR/oxipub
+	mkdir $OPENBETLOGDIR/oxirep
+	mkdir $OPENBETLOGDIR/rep_client
+	mkdir $OPENBETLOGDIR/site_management
 
-CVSROOT=":pserver:shazleto@pserver:/cvsroot-openbet"
+	cd $OPENBETRELEASE
 
-# OXi
-cvs co -r R_shared2_27-0            OXi/shared
-cvs co -r R_dbPublish3_31_WH_1-2    OXi/services/dbPublish
-cvs co -r R_RT29669_repServer2_5-50 OXi/services/repServer
-cvs co -r T_Support_WIL-18130_2     OXi/services/repClient
+	CVSROOT=":pserver:shazleto@pserver:/cvsroot-openbet"
 
-# shared code
-cvs co -d shared_tcl -r B_WillHill_34_0_2 openbet/shared_tcl
-cvs co -d shared_pkg -r B_WillHill_34_0_2 openbet/shared_pkg
+	# OXi
+	cvs co -r R_shared2_27-0            OXi/shared
+	cvs co -r R_dbPublish3_31_WH_1-2    OXi/oxipubserver
+	cvs co -r R_RT29669_repServer2_5-50 OXi/oxirepserver
+	cvs co -r T_Support_WIL-18130_2     OXi/services/repClient
 
-# configuration
-CVSROOT=":pserver:shazleto@pserver:/cvsroot"
-cvs co -d conf                  -r B_WillHill_34_0_2 willhill/conf/global_func.cfg
-cvs co -d conf/OXi/oxirepserver -r B_WillHill_34_0_2 willhill/conf/OXi/oxirepserver 
-cvs co -d conf/OXi/oxipubserver -r B_WillHill_34_0_2 willhill/conf/OXi/oxipubserver 
-cvs co -d conf/postgres         -r B_WillHill_34_0_2 willhill/conf/postgres
+	# shared code
+	cvs co -d shared_tcl -r B_WillHill_34_0_2 openbet/shared_tcl
+	cvs co -d shared_pkg -r B_WillHill_34_0_2 openbet/shared_pkg
 
-# site management
-mkdir /tmp/ops; cd /tmp/ops
-wget http://artifactory.ci01.openbet/artifactory/simple/openbet-tcl-release-local/com/openbet/ops/openbet-ops/1.2/openbet-ops-1.2-sources.tgz
-tar -xzvf openbet-ops-1.2-sources.tgz
-mv site_management $OPENBETRELEASE
-cd $OPENBETRELEASE
-rm -rf /tmp/ops
-chmod +x $OPENBETRELEASE/site_management/obcontrol
-chmod +x $OPENBETRELEASE/site_management/obmonitor
-chmod +x $OPENBETRELEASE/site_management/obmanage
+	# configuration
+	CVSROOT=":pserver:shazleto@pserver:/cvsroot"
+	cvs co -d conf                  -r B_WillHill_34_0_2 willhill/conf/global_func.cfg
+	cvs co -d conf/OXi/oxirepserver -r B_WillHill_34_0_2 willhill/conf/OXi/oxirepserver 
+	cvs co -d conf/OXi/oxipubserver -r B_WillHill_34_0_2 willhill/conf/OXi/oxipubserver 
+	cvs co -d conf/postgres         -r B_WillHill_34_0_2 willhill/conf/postgres
+}
 
-# links
-ln -s $OB/setup/conf/oxipub.cfg                              $OPENBETRELEASE/OXi/services/dbPublish
-ln -s $OPENBETRELEASE/OXi/services/repServer/schema-base.cfg $OPENBETRELEASE/OXi/services/dbPublish
-ln -s $OPENBETRELEASE/OXi/services/repServer/api-base.cfg    $OPENBETRELEASE/OXi/services/dbPublish
-ln -s $OPENBETRELEASE/OXi/shared/tcl                         $OPENBETRELEASE/OXi/services/dbPublish/tcl/shared
-ln -s $OPENBETRELEASE/shared_tcl                             $OPENBETRELEASE/OXi/services/dbPublish/tcl
-ln -s $OPENBETRELEASE/shared_pkg                             $OPENBETRELEASE/OXi/services/dbPublish/tcl
-ln -s $OB/setup/conf/oxirep.cfg                              $OPENBETRELEASE/OXi/services/repServer
-ln -s $OPENBETRELEASE/OXi/shared/tcl                         $OPENBETRELEASE/OXi/services/repServer/tcl/shared
-ln -s $OPENBETRELEASE/shared_tcl                             $OPENBETRELEASE/OXi/services/repServer/tcl
-ln -s $OPENBETRELEASE/shared_pkg                             $OPENBETRELEASE/OXi/services/repServer/tcl
+site_management() {
+	# remove if it exsits already
+	rm -rf $OPENBETRELEASE/site_management
+	mkdir /tmp/ops; cd /tmp/ops
+	wget http://artifactory.ci01.openbet/artifactory/simple/openbet-tcl-release-local/com/openbet/ops/openbet-ops/1.2/openbet-ops-1.2-sources.tgz
+	tar -xzvf openbet-ops-1.2-sources.tgz
+	mv site_management $OPENBETRELEASE
+	cd $OPENBETRELEASE
+	rm -rf /tmp/ops
+	chmod +x $OPENBETRELEASE/site_management/obcontrol
+	chmod +x $OPENBETRELEASE/site_management/obmonitor
+	chmod +x $OPENBETRELEASE/site_management/obmanage
+}
 
-# hacks
-# see OPENBETRELEASEPUB-458?
-sed -i 's/OXi::log::init      DEFAULT/OXi::log::init/g' $OPENBETRELEASE/OXi/services/repServer/tcl/init.tcl
+links() {
+	# links
+	ln -s $OB/setup/conf/oxipub.cfg                        $OPENBETRELEASE/OXi/oxipubserver
+	ln -s $OPENBETRELEASE/OXi/oxirepserver/schema-base.cfg $OPENBETRELEASE/OXi/oxipubserver
+	ln -s $OPENBETRELEASE/OXi/oxirepserver/api-base.cfg    $OPENBETRELEASE/OXi/oxipubserver
+	ln -s $OPENBETRELEASE/shared_tcl                       $OPENBETRELEASE/OXi/oxipubserver/tcl
+	ln -s $OPENBETRELEASE/shared_pkg                       $OPENBETRELEASE/OXi/oxipubserver/tcl
+	ln -s $OB/setup/conf/oxirep.cfg                        $OPENBETRELEASE/OXi/oxirepserver
+	ln -s $OPENBETRELEASE/shared_tcl                       $OPENBETRELEASE/OXi/oxirepserver/tcl
+	ln -s $OPENBETRELEASE/shared_pkg                       $OPENBETRELEASE/OXi/oxirepserver/tcl
+}
+
+hacks() {
+	# hacks
+	# see OPENBETRELEASEPUB-458?
+	sed -i 's/OXi::log::init      DEFAULT/OXi::log::init/g' $OPENBETRELEASE/OXi/oxirepserver/tcl/init.tcl
+}
+
+site_management
+hacks
