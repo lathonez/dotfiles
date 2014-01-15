407eed0df4d4f1c60ea171eb21ab2ecfb28d8b73
Stephen Hazleton <stephen.hazleton@openbet.com>
2014-01-15 10:39:08 +0000

    modifying drun

--- scripts/drun.sh
+++ scripts/drun.sh
@@ -13,18 +13,15 @@ g=`which grep`
 # this is filth, cant think of a better way of doing it for now - hostname would be preferably, but then we have to use custproxy for the DNS, which may break SSL
 host_ipa=`ifconfig | $g -A1 eth | $g inet | $g -v inet6 | awk -F ':' '{print $2}' | awk '{print $1}'`
 conthost="test1"
-ssh_port=40000
-web_port=50000
-ssl_port=50001
+ssh_port=50010
+web_port=50011
+ssl_port=50012
 
 if [ "$2" = "" ]
 then
-	cmd="/root/start_hills.sh $web_port $ssl_port $host_ipa"
+	cmd="/root/start_hills.sh $host_ipa $web_port $ssl_port"
 else
 	cmd=$2
 fi
 
-# sudo docker run -privileged -h=$hostname -p $ssh_port:22 -p $web_port:80 -p $ssl_port:443 -t -i $1 $cmd
-echo "sudo docker run -privileged -h $conthost -p $ssh_port:22 -p $web_port:80 -p $ssl_port:443 -t -i $1 $cmd"
-
-sudo docker run -privileged -h $conthost -p $ssh_port:22 -p $web_port:80 -p $ssl_port:443 -t -i $1 /bin/bash
+sudo docker run -privileged -h $conthost -p $ssh_port:22 -p $web_port:80 -p $ssl_port:443 -t -i $1 $cmd
