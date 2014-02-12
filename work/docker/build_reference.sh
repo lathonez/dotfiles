8e74bf3a8aff9666b53b8c8b4b3d5775ed61bb34
Stephen Hazleton <stephen.hazleton@openbet.com>
2014-02-12 14:39:12 +0000

    adding docker scripts

---
+++ work/docker/build_reference.sh
@@ -0,0 +1,2 @@
+nohup python shutit_main.py --shutit_module_path ../shutit_modules/openbet/ > /tmp/shutit.log &
+tail -f /tmp/shutit.log
---
+++ work/docker/start_registry.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -name 10.199.1.136 -p 5000:5000 -v /srv/registry:/srv/registry imiell/registry /docker-registry/run.sh
