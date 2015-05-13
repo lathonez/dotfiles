6afcf74fdd6592add8f6e5b9c736beb0176977e7
Stephen Hazleton <shazleto@gmail.com>
2015-05-13 14:02:33 +0100

    Adding feh to install and fixing whitespacew in xdefaults

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,22 +5,27 @@
 #
 
 # alphabetical list of apps to install
-apps="docker.io git gnome-panel google-chrome-stable pip python scrot tcl rxvt-unicode vim vlc xmonad"
+apps="docker.io feh git gnome-panel google-chrome-stable python python-pip scrot tcl rxvt-unicode vim vlc xmonad"
 
-# add chrome to sources
+# shouldn't be run as root else all the links will be owned by root!
+if [ `whoami` = "root" ]; then
+	echo "Don't run this as root please"
+	exit
+fi
 
 dpkg -s google-chrome-stable 2&> /dev/null
 chrome_installed=$?
 
-if [ $chrome_installed -eq 1 ]; then
-	echo "Adding chrome to sources.."
-	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-fi
+# add chrome to sources
+#if [ $chrome_installed -eq 1 ]; then
+#	echo "Adding chrome to sources.."
+#	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#fi
 
 echo "Updating system.."
-sudo apt-get -y update
-sudo apt-get -y upgrade
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
 
 for app in $apps; do
 	echo "Installing ${app}.."
@@ -40,10 +45,10 @@ echo "Resetting symlinks"
 mkdir -p ~/.xmonad
 rm -f ~/.bashrc && ln -s ~/utils/bash/bash_core ~/.bashrc
 rm -f ~/.vimrc  && ln -s ~/utils/vim/.vimrc ~/.vimrc
-rm -f ~/.xmonad/Xresources && ln -s ~/utils/xmonad/Xresources ~/.xmonad/Xresources
-rm -f ~/.xmonad/xmonad.hs  && ln -s ~/utils/xmonad/xmonad.hs  ~/.xmonad/xmonad.hs
-rm -f ~/.xmonad/xmobarrc   && ln -s ~/utils/xmonad/xmobarrc   ~/.xmonad/xmobarrc
+rm -f ~/.Xresources       && ln -s ~/utils/xmonad/Xresources ~/.Xresources
+rm -f ~/.Xdefaults        && ln -s ~/utils/xmonad/Xdefaults  ~/.Xdefaults
+rm -f ~/.xmonad/xmonad.hs && ln -s ~/utils/xmonad/xmonad.hs  ~/.xmonad/xmonad.hs
+rm -f ~/.xmonad/xmobarrc  && ln -s ~/utils/xmonad/xmobarrc   ~/.xmonad/xmobarrc
 
 # recompile xomand
 xmonad --recompile
-rm -f ~/.xmonad/Xdefaults  && ln -s ~/utils/xmonad/Xdefaults  ~/.xmonad/Xdefaults
