3dfcbb4ca6681979ee6558542956a14bf996a58c
Stephen Hazleton <shazleto@gmail.com>
2015-06-12 19:53:56 +1200

    fixing this, works well now

--- scripts/mac_switch.sh
+++ scripts/mac_switch.sh
@@ -1,11 +1,8 @@
-interface="eth0"
-giles="78:2B:CB:AF:80:FB"
-phone="00:08:5D:74:DC:C4"
-tatty="f0:de:f1:46:94:94"
+interface="wlan0"
+mac="10:0B:A9:2F:16:50"
 
-# set which mac
-mac=$giles
-
+sudo service network-manager stop
 sudo ifconfig $interface down
 sudo ifconfig $interface hw ether $mac
 sudo ifconfig $interface up
+sudo service network-manager start
