046706bf4bbea5969336c5cdbb4e073eb96e2648
lathonez <lathonez@snappy.io>
2025-09-22 00:37:11 +0100

    UPL

---
+++ scripts/update-plex-libs.sh
@@ -0,0 +1,2 @@
+curl -G "http://localhost:32400/library/sections/1/refresh" --data-urlencode "X-Plex-Token=zpFAPXzzxtRrc-PgxRUe"
+curl -G "http://localhost:32400/library/sections/2/refresh" --data-urlencode "X-Plex-Token=zpFAPXzzxtRrc-PgxRUe"
